6696aea3ba146af9a7947250c52f754c04c21656
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-04-05 04:28:24 -0400

    Finally works!

--- Web_Dev/웹/centos/html_table_sql-test.php
+++ Web_Dev/웹/centos/html_table_sql-test.php
@@ -9,8 +9,6 @@
 
 	//print "html_table_sql.php loaded";
 
-    print '<!DOCTYPE html><head><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Point Board</title><link rel="stylesheet" type="text/css" href="html_table_sql.css"></head>';
-
 	$tot=0;
     $tot_result=array(
         array(),
@@ -60,17 +58,12 @@
         //wprint "</tr>";
 		$tot++;
 	}
-  print '<body><div class="topnav"><a href="index.html">Main Console</a>
-          <a class="active" href="">Ranking Board</a><a href="https://www.temsacademy.com/contact-us">Contact</a><a href="https://www.temsacademy.com/about">About</a></div><div class="block"></div>';
-    echo '<script type="text/javascript" src="html_table_sql.js"></script>';
     //array_multisort($sort, SORT_ASC, SORT_STRING,$tot_result);
     $sort_sel=1;
     $previous=0;
     $sorted_element=0;//0:id, 1:name, 2:birthday, 3:age
     $sorted_increase_decrease=0;//0: increase, 1: decrease
 
-
-    print '<div><table id="myTable" border="2"><tr><th align="center"><button id="name_sec" onclick="sortTable2();" style="width:100%; height:25px; font-size:10.2pt"><strong>name</strong></button></th><th align="center" width="85"><button id="age_sec" onclick="sortTable3();" style="width:100%; height:25px; font-size:10.2pt"><strong>Age</strong></button></th><th align="center"><button id="point_sec" onclick="sortTable4();" style="width:100%; height:25px; font-size:10.2pt"><strong>Point</strong></button></th></tr>';//<th align="center" width="40"><button id="id_sec" onclick="sortTable1_1();" style="width:100%; height:25px; font-size:10.2pt"><strong>id &#62;</strong></button></th>
     for($i=0;$i<$tot;$i++){
         print "<tr>" ;
         //print "<td align='center'>" .$tot_result[$i][0]."</td>";
@@ -79,7 +72,6 @@
         print "<td align='center'>" .$tot_result[$i][3]."</td>";
         print "</tr>" ;
     }
-    print '</table></div>';
     /*if(count($_COOKIE)> 0) {
         echo "Cookies are enabled.";
     } else {
@@ -100,8 +92,4 @@
         }
     }*/
     mysqli_close($conn);
-print '<div class="block"/>';
-    print '<div class="footer"><p>&copy; Copyright <script type="text/javascript">var d = new Date()document.write(d.getFullYear())</script>, Canada TEMS Academy<br><img src="https://storage.googleapis.com/tems_point_system_image_storage_2/52dee1_8a31d53908ce2a3ee4eb3194319ff85b.png" width="80px" alt="TEMS LOGO"></p><p align="right" class="ex1">Webpage created by Hongjun Yun<br>hongjun.yun@icloud.com</p><div>';
-    //print "<br><a href='table.html'>Main screen</a>";
-    print "</body></html>";
 ?>
