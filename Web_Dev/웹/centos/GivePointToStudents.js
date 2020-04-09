915f277c423ad1f84bb6e9995ca77975819bd06c
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-04-08 23:55:17 -0400

    Need to fix login page
    Test on Galaxy A5 2017, Bottom footer is commin up

--- Web_Dev/웹/centos/GivePointToStudents.js
+++ Web_Dev/웹/centos/GivePointToStudents.js
@@ -1,4 +1,18 @@
 function myFunction() {
-  var x = document.getElementById("SelectedName").value;
-  document.getElementById("blankArea1").innerHTML = "You selected: " + x;
+    var x = document.getElementById("SelectedName").value;
+    var IdforText = "LoadedPoint" + (parseInt(x));
+    var selectedPoint = document.getElementById(IdforText).innerHTML;
+    document.getElementById("currentPoint").value = selectedPoint;
+}
+
+function myCalc(num) {
+    var x = document.getElementById("currentPoint").value;
+    var result = (parseInt(x) + num);
+    document.getElementById("currentPoint").value = result;
+}
+
+function myDoubleCalc() {
+    var x = document.getElementById("currentPoint").value;
+    var result = (parseInt(x) * 2);
+    document.getElementById("currentPoint").value = result;
 }
--- Web_Dev/웹/centos/GivePointToStudents.php
+++ Web_Dev/웹/centos/GivePointToStudents.php
@@ -64,14 +64,27 @@
     echo '<script type="text/javascript" src="GivePointToStudents.js"></script>';
     echo '<div class="block"></div>';
 
+    print '<div style="display:none"><table id="myTable" border="2"><tr><th align="center">name</th><th align="center" width="85">Age</th><th align="center">Point</th></tr>';
+    for($i=0;$i<$tot;$i++){
+        print "<tr>" ;
+        //print "<td align='center'>" .$tot_result[$i][0]."</td>";
+        print "<td align='center'>" .$tot_result[$i][1]."</td>";
+        print "<td align='center'>" .$tot_result[$i][2]."</td>";
+        print "<td align='center'><p id='LoadedPoint$i'>" .$tot_result[$i][3]."</p></td>";
+        print "</tr>" ;
+    }
+    print "<tr><td align='center'>Name</td><td align='center'>18</td><td align='center'><p id='LoadedPoint99'>20</p></td></tr>";
+    print '</table></div>';
+
 
     print '<center><div class="block"><select name="Name" id="SelectedName" onchange="myFunction()"><option selected value="0">please select the student</option>';//<th align="center" width="40"><button id="id_sec" onclick="sortTable1_1();" style="width:100%; height:25px; font-size:10.2pt"><strong>id &#62;</strong></button></th>
     for($i=0;$i<$tot;$i++){
-        print '<option value="'.($i+1).'">';
+        print '<option value="'.$i.'">';
         print $tot_result[$i][1];
         print "</option>";
     }
-    print '</select></div></center>';
+    print '</select><br><label for="currentPoint">Current point for Selected Student: </label><input type="text" id="currentPoint" name="currentPoint" placeholder="Point">';
+    print '<br><button id="M10" onclick="myCalc(-10)">-10</button><button id="M5" onclick="myCalc(-5)">-5</button><button id="P5" onclick="myCalc(5)">+5</button><button id="P10" onclick="myCalc(10)">+10</button><button id="P15" onclick="myCalc(15)">+15</button><button id="P20" onclick="myCalc(20)">+20</button><button id="P50" onclick="myCalc(50)">+50</button><button id="Double" onclick="myDoubleCalc()">X2</button></div></center>';
     mysqli_close($conn);
 print '<div id="blankArea1" class="block"></div>';
     print '<div class="footer"><p>&copy; Copyright <script type="text/javascript">var d = new Date();document.write(d.getFullYear())</script>, Canada TEMS Academy<br><img src="https://storage.googleapis.com/tems_point_system_image_storage_2/52dee1_8a31d53908ce2a3ee4eb3194319ff85b.png" width="80px" alt="TEMS LOGO"></p><p align="right" class="ex1">Webpage created by Hongjun Yun<br>hongjun.yun@icloud.com</p></div>';
